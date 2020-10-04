d5b881f3fe9978d193973f3fbead6c5174568e11
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-04 12:38:22 +0200

    Daemon thread

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@
 // In this case the compiler treats Main as a thread class.
 public class Main extends Thread{
     public void run(){
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(Thread.currentThread().getPriority());
+        if (Thread.currentThread().isDaemon()){
+            System.out.println("Daemon Thread: "
+                    + currentThread().getName());
+        }else{
+            System.out.println("User thread: " +
+                    currentThread().getName());
+        }
     }
     public static void main(String[] args) {
         Main m = new Main();
         Main m2 = new Main();
 
-        System.out.println("m = " + m.getName() + "(10)");
-        System.out.println("m2 = " + m2.getName() + "(5)");
-        System.out.println();
-
-        m.setPriority(10);
-        m2.setPriority(5);
-
+        m.setDaemon(true);
         m.start();
+
         m2.start();
     }
 }
