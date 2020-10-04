c9a984d7623b1b4207771396ec0064a5407d5889
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-04 10:37:18 +0200

    Testing join() method.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,29 @@
 // In this case the compiler treats Main as a thread class.
 public class Main extends Thread{
     public void run(){
-        System.out.println("In the name of Allah.");
+        for(int i=0; i<10; i++){
+            try {
+                Thread.sleep(100);
+                System.out.println( Thread.currentThread().getName() +"  "+ i);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
     public static void main(String[] args) {
         Main m = new Main();
+        Main m2 = new Main();
+        Main m3 = new Main();
+
         m.start();
+        try {
+            m.join();
+            System.out.println("(m) Thread object Finished.");
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        m2.start();
+        m3.start();
     }
 }
